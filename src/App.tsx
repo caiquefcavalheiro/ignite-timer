346bca98c56cf52372cdb4bc68b0705cd29506a3
Caique F. Cavalheiro <caiquefcavalheiro@gmail.com>
2023-06-30 09:55:51 -0300

    feat: created the button component and its variations with styledComponents

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { Button } from "./components/Button";
+
 export function App() {
   return (
     <div>
-      <h1>Hello World!</h1>
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="danger" />
+      <Button variant="success" />
     </div>
   );
 }
